f073950628581803f820faaea6e64218a40d59ed
SwaVan <swavan@evan-book.local>
2021-04-19 18:28:09 -0400

    fixed PyQT app

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from shared.recorder import SwaVanLogRecorder
 from shared.widgets.builder import full_path
 
 if __name__ == "__main__":
-    SwaVanLogRecorder.init()
     app = QApplication(sys.argv)
     app.setApplicationDisplayName("SwaVan")
     app.setApplicationName("SwaVan")
@@ -24,4 +23,5 @@ if __name__ == "__main__":
 
     widget = SwaVanMainWindow()
     widget.show()
+    SwaVanLogRecorder.init()
     sys.exit(app.exec())
